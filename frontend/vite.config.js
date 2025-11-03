d872af0fecfea262485a050c85c1bbda8e3b8aa4
RajalekshmiRe <rajalekshmireji07@gmail.com>
2025-11-03 20:49:57 +0530

    Initial commit : Excel Analytics Platform

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,20 +3,21 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
+process.env.TAILWIND_DISABLE_COLORS = 'true'; // 👈 Force Tailwind to output RGB colors
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // <-- alias for src
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5000", // your backend
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
   },
 });
-
