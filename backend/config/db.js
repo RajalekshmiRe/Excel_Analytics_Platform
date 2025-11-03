02716cec6a06da5ca178c310b8985ce4c7e17151
RajalekshmiRe <rajalekshmireji07@gmail.com>
2025-11-03 20:43:20 +0530

    Initial commit : Excel Analytics Platform

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,18 +9,3 @@ export default async function connectDB(uri) {
     process.exit(1);
   }
 }
-// import mongoose from "mongoose";
-
-// export default async function connectDB() {
-//   const password = encodeURIComponent("72003"); // Atlas password
-//   const uri = `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASSWORD)}@cluster0.o20s8fb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-
-//   try {
-//     await mongoose.connect(uri);
-//     console.log("✅ MongoDB connected");
-//   } catch (err) {
-//     console.error("MongoDB connection error:", err.message);
-//     process.exit(1);
-//   }
-// }
